f987c0d423dd41b03d4af2aa18d0861da60290c6
olivier615 <52729795+olivier615@users.noreply.github.com>
2021-08-16 17:28:56 +0800

    Update 2021-08-16T09:28:52.759Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,11 +4,29 @@ $(function () {
   console.log('Hello Bootstrap5');
 });
 var swiper = new Swiper(".mySwiper", {
+  // loop:true,
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    clickable: false
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    }
   }
 });
 //# sourceMappingURL=all.js.map
