6725c22d02e2fc2ed6c2c476c9adb5343a350e55
olivier615 <52729795+olivier615@users.noreply.github.com>
2021-08-16 16:51:33 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,13 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView:3,
+    spaceBetween: 30,
+    pagination: {
+      el:".swiper-pagination",
+      clickable: true,
+    },
+  });
