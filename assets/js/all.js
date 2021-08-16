ed0a593414516b44ee31e962720e0dc2cfb00d1a
olivier615 <52729795+olivier615@users.noreply.github.com>
2021-08-16 16:55:20 +0800

    Update 2021-08-16T08:55:14.581Z

---
+++ assets/js/all.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+//# sourceMappingURL=all.js.map
